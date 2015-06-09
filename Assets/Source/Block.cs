c523758d4bac649be53f91ecf3ac88d468786d9e
darkmoo <wardm17@gmail.com>
2015-06-09 07:49:42 -0400

    Block Basics

---
+++ Assets/Source/Block.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	public int _hitsRemaining, _baseHits;
+	
+	public int hitsRemaining {
+		get { return _hitsRemaining; }
+		set { _hitsRemaining = value; }
+	}
+	
+	public int baseHits {
+		get { return _baseHits; }
+	}
+	
+	
+	public void BlockInit(int value) {
+		_baseHits = value;
+		_hitsRemaining = value;
+	}
+	
+	public int modifyHits(int value) {
+		_hitsRemaining += value;
+		
+		if (_hitsRemaining <= 0) {
+			_hitsRemaining = 0; // keep from going negative
+		}
+		
+		return _hitsRemaining;
+	}
+	
+	public void destroyBlock() {
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+		spriteRenderer.enabled = false;
+	}
+}
---
+++ Assets/Source/BlockManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BlockManager : MonoBehaviour {
+
+	public GameObject BlockPrefab;
+
+	public int levelWidth, levelHeight;
+
+	public List<GameObject> BlockList = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+		//SpriteRenderer blockRenderer = BlockPrefab.GetComponent<SpriteRenderer> ();
+		
+		levelWidth = Screen.width / (64);
+		levelHeight = Screen.height / (64);
+		generateBlocks ();
+	}
+	
+	// Update is called once per frame
+		void Update () {
+	}
+
+
+	private void generateBlocks() {
+		for(int height = 1; height <= levelHeight; height++){
+			for(int width = 1; width <= levelWidth; width++){
+				Instantiate(BlockPrefab,Camera.main.ScreenToWorldPoint(new Vector3(((int)(Screen.width / 1.1)) - (width * 50),(Screen.height - (height * 50)),10)),Quaternion.identity);
+			}
+		}
+	}
+}
