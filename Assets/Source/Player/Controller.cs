cbad4c128e744548419ffb9d4ef872ffd374f49d
gobbo1008 <technofreak91@web.de>
2015-06-12 16:36:36 +0200

    I feex
    ...I hope :s

--- Assets/Source/Player/Controller.cs
+++ Assets/Source/Player/Controller.cs
@@ -5,22 +5,9 @@ public class Controller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		EdgeCollider2D theEdge = GetComponent<EdgeCollider2D>();
-		Vector2[] points = theEdge.points;
-		
-		Vector2[] newPoints = new Vector2[6];
-		newPoints[0] = new Vector2(-2.6f, 0.2f);
-		newPoints[1] = new Vector2(-2f, 0.5f);
-		newPoints[2] = new Vector2(-1.4f, 0.7f);
-		newPoints[3] = new Vector2(1.4f, 0.7f);
-		newPoints[4] = new Vector2(2f, 0.5f);
-		newPoints[5] = new Vector2(2.6f, 0.2f);
-		
-		for (int i = 0; i <= 5; i++) {
-			points[i] = newPoints[i];
-		}
-		
-		theEdge.points = points;
+		float level = Camera.main.ViewportToWorldPoint(new Vector3(0, 0.1f, 0)).y;
+		Vector2 pos = new Vector2(transform.position.x, level);
+		transform.position = pos;
 	}
 	
 	// Update is called once per frame
@@ -30,7 +17,7 @@ public class Controller : MonoBehaviour {
 		float distance = 0;
 		float buffer = GetComponent<Renderer>().bounds.size.x/2;
 		if (targetPlane.Raycast (ray, out distance)) {
-			Vector3 pos = new Vector3(Mathf.Clamp(ray.GetPoint(distance).x + buffer, Camera.main.ViewportToWorldPoint(Vector3.zero).x + 2*buffer, -Camera.main.ViewportToWorldPoint(Vector3.zero).x) - buffer, transform.position.y, 0);
+			Vector2 pos = new Vector2(Mathf.Clamp(ray.GetPoint(distance).x + buffer, Camera.main.ViewportToWorldPoint(Vector3.zero).x + 2*buffer, -Camera.main.ViewportToWorldPoint(Vector3.zero).x) - buffer, transform.position.y);
 			transform.position = pos;
 		}
 	}
